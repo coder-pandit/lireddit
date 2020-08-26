0e7879d86094fa79699d59514e7145499ec124fb
Shiv Kumar <kumarshivsharma12@gmail.com>
2020-08-27 00:31:59 +0530

    apollo-server-express-setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,30 @@
+import express from 'express';
 import { MikroORM } from '@mikro-orm/core';
-import { __prod__ } from './constants';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { __prod__, __port__ } from './constants';
 import { Post } from './entities/Post';
-import microConfig from './mikro-orm.config';
+import mikroOrmConfig from './mikro-orm.config';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
-  const orm = await MikroORM.init(microConfig);
+  const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: "my first post" });
-  // await orm.em.persistAndFlush(post);
 
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(__port__, () => {
+    console.log('Server started on localhost:4000');
+  });
 };
 
-main().catch(err => {
-  console.error(err);
-});
+main();
