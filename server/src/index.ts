831c8eb591e81c0f52ada3e1bc2900847aab5953
Shiv Kumar <kumarshivsharma12@gmail.com>
2020-08-27 01:09:08 +0530

    mikro-orm-graphql-crud

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,9 @@ import { MikroORM } from '@mikro-orm/core';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 import { __prod__, __port__ } from './constants';
-import { Post } from './entities/Post';
 import mikroOrmConfig from './mikro-orm.config';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
@@ -15,9 +15,10 @@ const main = async () => {
 
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, PostResolver],
       validate: false
-    })
+    }),
+    context: () => ({ em: orm.em })
   });
 
   apolloServer.applyMiddleware({ app });
