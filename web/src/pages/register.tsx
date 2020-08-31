d1023a4e6372fb7f4e6d33fdd95efe3c7220c312
Shiv Kumar <kumarshivsharma12@gmail.com>
2020-09-01 01:07:15 +0530

    Show register-errors

--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import { Box, Button } from '@chakra-ui/core';
+import { useRouter } from 'next/router';
 import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
 import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
 
 interface RegisterProps {}
 
 const Register: React.FC<RegisterProps> = () => {
+  const router = useRouter();
   const [, register] = useRegisterMutation();
 
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: '', password: '' }}
-        onSubmit={async values => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
+          console.log(response);
+
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push('/');
+          }
         }}
         render={({ isSubmitting }) => (
           <Form>
